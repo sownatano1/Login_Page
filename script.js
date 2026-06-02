2484fbedac4d45595bf33a85d225ea5d553497fe
sownatano1 <161262646+sownatano1@users.noreply.github.com>
2026-06-02 18:26:58 -0300

    JavaScript implementado, mensagem de erro

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-alert("Oi")
+function Submit(){
+    let email = document.getElementById("email").value.trim()
+    let password = document.getElementById("psw").value.trim()
+
+    let paragraph = document.createElement("p")
+
+    const container = document.getElementById("incorrect-container")
+    const count = container.children.length
+
+    const button = document.getElementById("submit")
+
+    button.classList.add("active")
+    setTimeout(() => button.classList.remove("active"), 150)
+
+    if (count >= 1){
+        container.removeChild(container.lastChild)
+    }
+
+    if (email == "admin@" && password == "1234"){
+        window.location.hred = "page.html"
+    }
+
+    else{
+        container.appendChild(paragraph)
+        paragraph.textContent = "E-mail ou senha incorreta!"
+        paragraph.classList.add("incorrect")
+    }
+}
